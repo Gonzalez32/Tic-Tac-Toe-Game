d2c7b2f673eccb1c68248e97c9dedd207d9397bc
Gonzalez32 <3232rose@gmail.com>
2020-11-15 21:53:56 -0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 // Variables might include (board/turn/winner)
+let board
+let turn = 'X'
+let win
 
 /*------------------------ Cached Element References ------------------------*/
 
 // You might choose to put your game status here
+const squares = Array.from(document.querySelectorAll('#board div'))
+const messages = document.getElementById('message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 // This is where you should put the event listener
 // for a mouse-click
+document.getElementById('grid').addEventListener('click', switchOver)
+document.getElementById('resetBtn').addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 
